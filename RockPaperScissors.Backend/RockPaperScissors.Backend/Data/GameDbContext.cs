48d2ad27954b9cbcd475fd004a6b6cebe1dbaa4f
Benedek5@stud.uni-obuda.hu <benedek.szabo@stud.uni-obuda.hu>
2025-12-30 16:19:06 +0100

    Add DbContext and configure Program.cs

---
+++ RockPaperScissors.Backend/RockPaperScissors.Backend/Data/GameDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RockPaperScissors.Backend.Models;
+
+namespace RockPaperScissors.Backend.Data
+{
+    public class GameDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+        public GameDbContext(DbContextOptions<GameDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
--- RockPaperScissors.Backend/RockPaperScissors.Backend/Program.cs
+++ RockPaperScissors.Backend/RockPaperScissors.Backend/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using RockPaperScissors.Backend.Data;
+
 namespace RockPaperScissors.Backend
 {
     public class Program
@@ -10,21 +13,38 @@ namespace RockPaperScissors.Backend
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
+            builder.Services.AddDbContext<GameDbContext>(opt =>
+            {
+                opt.UseSqlServer(builder.Configuration["db:conn"]);
+            });
+
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+
+            if (builder.Environment.IsProduction())
+            {
+                builder.WebHost.ConfigureKestrel(options =>
+                {
+                    options.ListenAnyIP(int.Parse(builder.Configuration["settings:port"] ?? "6500"));
+                });
+            }
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
+            app.UseCors(t => t
+                .WithOrigins(builder.Configuration["settings:frontend"] ?? "http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .AllowAnyMethod());
 
             app.MapControllers();
 
